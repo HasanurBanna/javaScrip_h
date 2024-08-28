db7c26abe6359957658c2dab0e41ac6f5d61024e
Hasanur <hasanurbanna03@gmail.com>
2024-08-28 21:10:56 +0600

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,317 @@
+// 1 No:...........
+// Create a variable called carName, assign the value Volvo to it.
+
+let carName = "volvo" ; 
+
+
+
+// 2 No..........
+// On one single line, declare three variables with the following names and values:
+
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+
+let firstName = "John";         // write variable name
+let lastName = "Doe";           //write variable values 
+let age = 35 ;                  // write variable name and values both 
+
+
+// 3 No .............
+// Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+var x = 10;
+var y = 5;
+var x = x*y                      // what will be the value of x ? 
+
+console.log(x)
+// x = 50 hobe............
+
+
+
+
+// 4 No.............
+// Use comments to describe the correct data type of the following variables:
+
+let length = 16;                 // data typeof "number" ? 
+let lastNam = "Johnson";      // data typeof "string" ? 
+
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+};          // data typeof "object" ? 
+
+
+// 5 No.................
+// Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+// call the function and see the output 
+myFunction()
+
+
+//  6 No................
+// 1. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+
+const x = {
+    name: "John",
+    age: 50
+}
+alert(x.name + " " + "is" + " " + x.age);
+
+
+// 7 No.................
+// 1. The <button> element should do something when someone clicks on it. Try to fix it!
+
+<button>Click me.</button> 
+
+
+// 8 No..........................
+// 1. Alert the number of items in an array, using the correct Array property: 
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.n);
+
+
+// 2. Change the first item of Brand to "Ford".
+
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+Brand.unshift("ford")
+console.log(Brand)
+
+
+
+// 9 No.......................
+
+// 1. Use the correct Math method to create a random number.  
+
+Math.random()
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+
+Math.max()
+
+// 3. Use the correct Math method to get the square root of 9.
+
+
+// 10 No...............
+// 1. Choose the correct comparison operator to alert true, when x is greater than y. 
+x = 10;
+y = 5;
+
+alert() // ? 
+
+
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+
+
+
+
+
+
+
+
+
+
+// 11 No......................................
+// function celsiusToFahrenheit(celsius) {
+//     return ...;
+//   }
+  
+//   console.log(celsiusToFahrenheit(0)); // 32
+//   console.log(celsiusToFahrenheit(25)); // 77
+  
+
+
+
+
+// 12 No............................................
+function isEven(num) {
+    return ;
+  }
+  
+  console.log(isEven(4)); // true
+  console.log(isEven(7)); // false
+  
+  
+
+
+
+
+
+// 13 No.....................................
+function sum(a, b) {
+    return ;
+  }
+  
+  console.log(sum(3, 4)); // 7
+  console.log(sum(10, 20)); // 30
+  
+
+
+
+
+// 14 No....................................
+function findSmallestNum(arr) {
+    return ;
+  }
+  
+  console.log(findSmallestNum([3, 5, 1, 9])); // 1
+  console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+  
+  
+
+
+
+
+// 15 No.......................................
+function countVowels(str) {
+    let vowels = 'aeiouAEIOU';
+    return ;
+  }
+  
+  console.log(countVowels("hello world")); // 3
+  console.log(countVowels("Javascript")); // 3
+  
+
+
+
+
+// 16 No........................................
+function getFirstElement(arr) {
+    return ;
+  }
+  
+  console.log(getFirstElement([1, 2, 3])); // 1
+  console.log(getFirstElement(["a", "b", "c"])); // "a"
+  
+
+
+
+
+// 17 No..............................................
+function isArrayEmpty(arr) {
+    return ;
+  }
+  
+  console.log(isArrayEmpty([])); // true
+  console.log(isArrayEmpty([1, 2, 3])); // false
+  
+
+
+
+
+
+// 18 No..................................................
+function factorialize(num) {
+    if (num === 0 || num === 1) return 1;
+    return ;
+  }
+  
+  console.log(factorialize(5)); // 120
+  console.log(factorialize(7)); // 5040
+  
+
+
+
+
+
+// 19 No............................................
+function reverseString(str) {
+    return ;
+  }
+  
+  console.log(reverseString("hello")); // "olleh"
+  console.log(reverseString("world")); // "dlrow"
+  
+
+
+
+
+
+// 20 No............................................
+function toLowerCase(str) {
+    return ;
+  }
+  
+  console.log(toLowerCase("HELLO WORLD")); // "hello world"
+  console.log(toLowerCase("JavaScript")); // "javascript"
+
+  
+
+
+
+
+// 21 No..................................
+function stringLength(str) {
+    return ;
+  }
+  
+  console.log(stringLength("hello")); // 5
+  console.log(stringLength("world")); // 5
+  
+
+
+
+
+
+
+// 22 No............................
+function mergeArrays(arr1, arr2) {
+    return ;
+  }
+  
+  console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
+  console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
+  
+
+
+
+
+
+
+// 23 No.............................
+function getLastElement(arr) {
+    return ;
+  }
+  
+  console.log(getLastElement([1, 2, 3])); // 3
+  console.log(getLastElement(["a", "b", "c"])); // "c"
+  
+
+
+
+//  24 No..................................
+function getFirstCharacter(str) {
+    return ;
+  }
+  
+  console.log(getFirstCharacter("hello")); // "h"
+  console.log(getFirstCharacter("world")); // "w"
+  
+
+
+
+
+
+// 25 No........................................
+function sumArray(arr) {
+    return ;
+  }
+  
+  console.log(sumArray([1, 2, 3, 4])); // 10
+  console.log(sumArray([-1, -2, -3, -4])); // -10
+  console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
+  
+
+
+
